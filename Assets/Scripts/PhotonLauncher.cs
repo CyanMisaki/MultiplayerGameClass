636e85560534f1273776e6d4e65ef5de29a99706
CyanMisaki <fluffyshy@yandex.ru>
2022-06-29 00:36:57 +0300

    HW3

--- Assets/Scripts/PhotonLauncher.cs
+++ Assets/Scripts/PhotonLauncher.cs
@@ -1,21 +1,33 @@
+using System;
 using UnityEngine;
 using Photon.Pun;
+using System.Threading.Tasks;
+using Photon.Realtime;
+using Unity.Jobs;
+using UnityEditor.VersionControl;
+using UnityEngine.UI;
 
 public class PhotonLauncher : MonoBehaviourPunCallbacks
 {
-    void Awake()
+    public Action<bool> OnConnectionSuccess;
+    [SerializeField] private Button _disconnect;
+    [SerializeField] private Button _connect;
+    private void Awake()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
+        _disconnect.interactable = false;
+        _disconnect.gameObject.SetActive(false);
+        _disconnect.onClick.AddListener(Disconnect);
+        _connect.onClick.AddListener(Connect);
     }
 
-    void Start()
+    private void Disconnect()
     {
-        Connect();
+        PhotonNetwork.Disconnect();
     }
-
+    
     public void Connect()
     {
-
         if (PhotonNetwork.IsConnected)
         {
             PhotonNetwork.JoinRandomRoom();
@@ -25,20 +37,52 @@ public class PhotonLauncher : MonoBehaviourPunCallbacks
             PhotonNetwork.ConnectUsingSettings();
             PhotonNetwork.GameVersion = Application.version;
         }
+
+        ChangeButtons();
+    }
+
+    private void ChangeButtons()
+    {
+        _connect.gameObject.SetActive(!_connect.IsActive());
+        _disconnect.gameObject.SetActive(!_disconnect.IsActive());
+        
+        _connect.interactable = !_connect.interactable;
+        _disconnect.interactable = !_disconnect.interactable;
     }
 
     public override void OnConnectedToMaster()
     {
-        Debug.Log("Complete OnConnectedToMaster");
+        Debug.Log("Connected To Master");
+        
+        PhotonNetwork.JoinLobby();
     }
-    /*public override void OnJoinedLobby()
+    
+    public override void OnJoinedLobby()
     {
-        Debug.Log($"Complete OnConnectedToLobby {PhotonNetwork.InLobby}");
-        base.OnJoinLobby();
+        Debug.Log($"Connected To Lobby: {PhotonNetwork.CurrentLobby.Name}");
+        
+        PhotonNetwork.JoinOrCreateRoom(
+            $"{PhotonNetwork.CurrentLobby.Name}.roomName{SystemInfo.deviceUniqueIdentifier}", 
+            new Photon.Realtime.RoomOptions { MaxPlayers = 2, IsVisible = true },
+            Photon.Realtime.TypedLobby.Default);
     }
+    
     public override void OnJoinedRoom()
     {
-        Debug.Log($"Complete OnConnectedToLobby {PhotonNetwork.InRoom}");
-        base.OnJoinRoom();
-    }*/
+        Debug.Log($"Connected To Lobby: {PhotonNetwork.CurrentRoom.Name}");
+        OnConnectionSuccess?.Invoke(PhotonNetwork.InRoom);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        OnConnectionSuccess?.Invoke(PhotonNetwork.IsConnected);
+        Debug.Log($"Disconnected");
+        ChangeButtons();
+    }
+
+    private void OnDestroy()
+    {
+        _disconnect.onClick.RemoveAllListeners();
+        _connect.onClick.RemoveAllListeners();
+    }
 }
